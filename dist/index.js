ad3e828649a3a829d8d7627c533a52e9eea1b9f3
pawel <kissonion24@gmail.com>
2020-09-19 17:39:48 +0200

    ENable CORS

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,15 @@ var _express2 = _interopRequireDefault(_express);
 
 var _tips = require('./routes/tips.js');
 
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 app.use('/tips', _tips.router);
+app.use((0, _cors2.default)());
 
 var port = process.env.PORT;
 if (port == null || port == "") {
