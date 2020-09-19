b15bcfe195c5422e06ca19f57690a0d132380117
pawel <kissonion24@gmail.com>
2020-09-19 17:37:32 +0200

    ENable CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import {router as tipsRouter} from './routes/tips.js'
+import cors from 'cors'
 
 const app = express()
 app.use('/tips', tipsRouter)
+app.use(cors())
 
 
 let port = process.env.PORT
